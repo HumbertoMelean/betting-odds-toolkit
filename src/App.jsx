a1482546dae2f5b4991a6c6628aabd63c87aefa2
Humberto Melean <canal-ruse-ignore@duck.com>
2025-12-09 12:04:22 -0500

    start betting odds vite project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(null);
+
+  return (
+    <>
+      <p>Hello</p>
+    </>
+  );
+}
+
+export default App;
